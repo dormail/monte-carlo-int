3c566fed5e36da58ec55e47dc5d2a1aaa42ef634
Matthias Maile <matthias.maile@udo.edu>
2020-11-06 06:10:48 +0100

    random num gen method added to point class

--- hm3-surface/main.cpp
+++ hm3-surface/main.cpp
@@ -6,20 +6,41 @@
 
 constexpr double pi = 3.1415927;
 
-class point{
+class point
+{
 	public:
 		double x,y;
 
+		// constructor
 		point(double X, double Y) : x(X), y(Y){};
-		void print(){
-			std::cout << "x =\t" << x 
-				<< "\n y =\t" << y << std::endl;
+
+		// set both x and y to random values
+		void random (std::mt19937 gen,
+				double x_min, 
+				double x_max, 
+				double y_min,
+				double y_max) 
+		{
+			// two random num gens for x and y respectivly
+			std::uniform_real_distribution<> dis1(x_min,x_max);
+			std::uniform_real_distribution<> dis2(y_min,y_max);
+
+			x = dis1(gen);
+			y = dis2(gen);
 		}
+
+
+
+		// void print(){
+		// 	std::cout << "x =\t" << x 
+		// 		<< "\ny =\t" << y << std::endl;
+		// }
 };
 
-int main(){
+int main()
+{
 	point a = point(3,4);
-	a.print();
+
 	return 0;
 }
 
